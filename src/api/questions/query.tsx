5e22e3290b39cd952806fbbd06016f5c588d41d3
1wooseok <74036731+1wooseok@users.noreply.github.com>
2023-07-02 14:57:24 +0900

    질문시 질문 textarea 초기화

--- src/api/questions/query.tsx
+++ src/api/questions/query.tsx
@@ -1,37 +1,11 @@
 // 질문하기
-import { InfiniteData, useInfiniteQuery, useMutation, useQueryClient } from '@tanstack/react-query';
+import { InfiniteData, useInfiniteQuery, useMutation } from '@tanstack/react-query';
 import { PostQuestionPayload, PostQuestionRes, QuestionApiRes, QuestionModel } from './model';
 import ChattyClient from '../HttpClient';
 import { QUERY_KEY } from '~/constants/queryKey';
-import { toast } from 'react-toastify';
-import { AxiosError } from 'axios';
 
-export function usePostQuestion({ toggleModal }: { toggleModal: () => void }) {
-	const queryClient = useQueryClient();
-	const { mutate, isLoading: isMutating } = useMutation(
-		(payload: PostQuestionPayload): Promise<PostQuestionRes> => ChattyClient.post('/chatty', payload)
-	);
-
-	function mutQuestion(payload: PostQuestionPayload) {
-		mutate(payload, {
-			onSuccess: () => {
-				queryClient.invalidateQueries([QUERY_KEY.PROFILE, payload.username]);
-				toast('질문이 전송되었습니다.');
-				toggleModal();
-			},
-			onError: err => {
-				const error = err as AxiosError;
-				const errorMsg = error?.response?.data as string;
-
-				toast(errorMsg || '알수없는 오류가 발생했습니다.');
-			},
-		});
-	}
-
-	return {
-		mutQuestion,
-		isMutating,
-	};
+export function usePostQuestion() {
+	return useMutation((payload: PostQuestionPayload): Promise<PostQuestionRes> => ChattyClient.post('/chatty', payload));
 }
 
 // 답변 완료된 질문 가져오기
--- src/pages/Home/components/QuestionModalForm.tsx
+++ src/pages/Home/components/QuestionModalForm.tsx
@@ -5,6 +5,14 @@ import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { usePostQuestion } from '~/api/questions/query';
 import Spinner from '~/components/Loading/Spinner';
+import { QUERY_KEY } from '../../../constants/queryKey';
+import { toast } from 'react-toastify';
+import { AxiosError } from 'axios';
+import { useQueryClient } from '@tanstack/react-query';
+
+// todo: answered data scheme 바꾸기
+// todo: usename userId 위치 바꾸기
+// todo: 답변 등록후 입력창 초기화
 
 export interface ModalProps {
 	isOpen: boolean;
@@ -14,18 +22,38 @@ export interface ModalProps {
 export default function QuestionModalForm({ isOpen, toggleModal }: ModalProps) {
 	const { username = '' } = useParams();
 	const [content, setContent] = useState('');
-	const { mutQuestion, isMutating } = usePostQuestion({ toggleModal });
+	const queryClient = useQueryClient();
+	const { mutate, isLoading: isMutating } = usePostQuestion();
 
-	function onSubmit() {
-		mutQuestion({
-			content,
-			username,
-		});
+	function onSubmit(e: React.FormEvent) {
+		e.preventDefault();
+
+		mutate(
+			{
+				content,
+				username,
+			},
+			{
+				onSuccess: () => {
+					toast('질문이 전송되었습니다.');
+					toggleModal();
+					setContent('');
+					return queryClient.invalidateQueries([QUERY_KEY.PROFILE, username]);
+				},
+				onError: err => {
+					const error = err as AxiosError;
+					const errorMsg = error?.response?.data as string;
+
+					toast(errorMsg || '알수없는 오류가 발생했습니다.');
+				},
+			}
+		);
 	}
 
 	return !isOpen ? null : (
 		<Dimmer onClick={toggleModal}>
-			<div
+			<form
+				onSubmit={onSubmit}
 				className={
 					'popUp absolute z-40 bottom-0 left-1/2 w-screen  max-w-lg h-340 bg-white rounded-t-2xl p-20 flex flex-col gap-16 translate-x-[-50%]'
 				}>
@@ -59,7 +87,6 @@ export default function QuestionModalForm({ isOpen, toggleModal }: ModalProps) {
 				</div>
 
 				<button
-					onClick={onSubmit}
 					disabled={content.length === 0 || isMutating}
 					style={{ opacity: content.length === 0 ? 0.8 : 1 }}
 					className={
@@ -67,7 +94,7 @@ export default function QuestionModalForm({ isOpen, toggleModal }: ModalProps) {
 					}>
 					<span className={'text-white font-normal'}>{isMutating ? <Spinner color={'white'} /> : '확인'}</span>
 				</button>
-			</div>
+			</form>
 		</Dimmer>
 	);
 }
